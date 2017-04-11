92e3b3bd978f93c6f74cce11ffe5417c909c80b3
mehamasum <mehamasum@gmail.com>
2017-04-11 23:19:45 +0600

    home

--- home.php
+++ home.php
@@ -14,20 +14,119 @@
 
     <script src='https://api.mapbox.com/mapbox.js/v3.0.1/mapbox.js'></script>
     <link href='https://api.mapbox.com/mapbox.js/v3.0.1/mapbox.css' rel='stylesheet' />
+
     <style>
-        #map { margin-top:50px; position:absolute; top:0; bottom:0; width:100%; }
+
+        .star:before {
+            content: ' \2605';
+            font-size: 20px;
+            color: coral;
+        }
+
+        hr {
+            margin: 5px;
+        }
+
+        .circle-green:before {
+            content: ' \25CF';
+            font-size: 20px;
+            color: forestgreen;
+        }
+
+        .circle-red:before {
+            content: ' \25CF';
+            font-size: 20px;
+            color: indianred;
+        }
+
+        #map { margin-top:50px; position:absolute; top:0; bottom:0; width:100%; opacity: 0.8;}
+
+        #data {
+            padding: 10px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.8); transition: 0.3s;
+            background-color: white;
+            margin: 20px;
+            position: absolute;
+            bottom:0;
+            right: 0;
+            text-align:center;
+        }
+
+        @media all and (max-width:800px)
+        {
+            #data {
+                padding: 10px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.8); transition: 0.3s;
+                font-size: 0.9em;
+                background-color: white;
+                position: absolute;
+                bottom:0;
+                right: 0;
+                text-align:center;
+            }
+        }
+
+        .dropdown {
+            margin: 10px;
+        }
     </style>
 </head>
 <body>
     <?php include("includes/static_top.html"); ?>
+
     <div id='map'></div>
+
+    <div id="data">
+
+        <div class="dropdown">
+            <button class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">
+                Name of the Bus
+                <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <div>
+                <span><strong>Shahbag, Dhaka</strong></span><span style="margin-left: 10px; color: grey">22 min ago</span>
+                <br>
+                <span>4:30 PM Up trip</span>
+                <br>
+                <span class="circle-green"></span><span>10</span> <span style="margin-left: 10px" class="circle-red"></span><span>10</span>
+                <hr>
+
+                <div>
+                    <img src="img/default.png" alt="Avatar" style="width: 40px; height: 40px;float: left">
+                    <div >
+                        <span><strong>Meha Masum</strong></span><span style="margin-left: 5px; color: grey">CSE</span><br>
+                        <span class="star"></span><span>10</span>
+                    </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <hr>
+            <button class="btn btn-success">Upvote</button>
+            <button class="btn btn-danger">Report</button>
+
+        </div>
+
+    </div>
+
+
+
     <script>
         L.mapbox.accessToken = 'pk.eyJ1IjoibWVoYW1hc3VtIiwiYSI6ImNpdnhscWRvbzAyN2wyeXRhY2w1eGRvYngifQ.Cwlq3LDDRoBdsIZ_kQPHig';
         var map = L.mapbox.map('map', 'mapbox.streets')
-                .setView([23.8103, 90.4125], 13).addControl(L.mapbox.geocoderControl('mapbox.places', {
+                .setView([23.8103, 90.4125], 16).addControl(L.mapbox.geocoderControl('mapbox.places', {
                     autocomplete: true
                 }));
     </script>
+
 </body>
 <!-- Bootstrap core JavaScript -->
 <!-- Placed at the end of the document so the pages load faster -->
