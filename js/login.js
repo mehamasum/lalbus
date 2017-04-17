f60f1c38a22e15b2afe489839533c62f91ede888
mehamasum <mehamasum@gmail.com>
2017-04-17 22:56:24 +0600

    login

--- js/login.js
+++ js/login.js
@@ -66,20 +66,5 @@ function validateLogin() {
 }
 
 
-function isInteger(n) {
-    return !isNaN(parseInt(n));
-}
-
-function isDuReg(n) {
-    if(n.length==10) return true;
-    else return false;
-}
-
-function isBDMob(n) {
-    if(n.length==11) return true;
-    else return false;
-}
-
-
 
 
