7ae9995fb6ed9d55fcc1ee90ae3bb3aaa3a51018
fahim <fahim6119@gmail.com>
2017-04-26 16:35:30 +0600

    Updated Schedule page interface

--- js/schedule.js
+++ js/schedule.js
@@ -3,14 +3,14 @@ function search(busid,name) {
     var slot=document.getElementById("received_table");
     var busname_top=document.getElementById("busname_top");
     busname_top.innerHTML=name+" "+"<span class=\"caret\"></span>";
-
+    console.log(busid+" "+name);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             //this.responseText;
             var reply = this.responseText;
 
-            console.log(reply);
+            //console.log(reply);
             slot.innerHTML=reply;
 
         }
